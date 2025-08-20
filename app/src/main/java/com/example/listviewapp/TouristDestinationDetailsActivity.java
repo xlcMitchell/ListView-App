ad081ed85c28ab51edf0e14aab647086a2dd8273
Mitchell Travis <Kiwitiendatm@gmail.com>
2025-08-20 15:44:25 +1200

    Callback methods added

--- app/src/main/java/com/example/listviewapp/TouristDestinationDetailsActivity.java
+++ app/src/main/java/com/example/listviewapp/TouristDestinationDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.listviewapp;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TouristDestinationDetailsActivity extends AppCompatActivity {
-
+    private static final String TAG = "Details Lifecycle";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,4 +29,39 @@ public class TouristDestinationDetailsActivity extends AppCompatActivity {
         description.setText(destination.getDescription());
 
       }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: Activity is now visible");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: Activity has focus and is ready for user interaction");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: Another activity is taking focus (this one is partially obscured)");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: Activity is no longer visible");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart: Activity is restarting after being stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: Activity is being destroyed and removed from memory");
+    }
     }
