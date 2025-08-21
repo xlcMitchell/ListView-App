4afd86769dc0285d585c6b92d7f8ff00640319d7
Mitchell Travis <Kiwitiendatm@gmail.com>
2025-08-21 12:44:44 +1200

    added comments

--- app/src/main/java/com/example/listviewapp/MainActivity.java
+++ app/src/main/java/com/example/listviewapp/MainActivity.java
@@ -29,23 +29,28 @@ public class MainActivity extends AppCompatActivity {
 
         createDataList(data);
         destinationList = findViewById(R.id.myListView); //may need updating??
-        TouristDestinationAdapter adapter = new TouristDestinationAdapter(getApplicationContext(),data);
-        destinationList.setAdapter(adapter);
+        TouristDestinationAdapter adapter = new TouristDestinationAdapter(getApplicationContext(),data); //new Adapter object with our destination data
+        destinationList.setAdapter(adapter); //adapter set to the list view
 
+        //event listener for the destinations in the list view
         destinationList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(getApplicationContext(), TouristDestinationDetailsActivity.class);
                 intent.putExtra(getResources().getString(R.string.tourist_destination_object), data.get(i));
-                startActivity(intent);
+                startActivity(intent); //start new activity -- go to selected destination
             }
         });
     }
-
+    //Method below creates the list of all of the tourist destinations
+    //to be displayed on the activity_main
     private void createDataList(List<TouristDestination> data){
+        //retrieve destinations and store as arrays
         TypedArray imageResources = getResources().obtainTypedArray(R.array.images);
         String [] imageTitles = getResources().getStringArray(R.array.titles);
         String [] descriptions = getResources().getStringArray(R.array.descriptions);
+        //for loop to create a new TouristDestination object for each destination
+        //and add it to the List
         for(int i = 0; i < imageTitles.length; i++){
             TouristDestination item = new TouristDestination(imageResources.getResourceId(i, -1),
                     imageTitles[i],
--- app/src/main/java/com/example/listviewapp/TouristDestination.java
+++ app/src/main/java/com/example/listviewapp/TouristDestination.java
@@ -3,7 +3,7 @@ package com.example.listviewapp;
 import java.io.Serializable;
 
 //serializable allows you to pass object of a class between activities
-
+//Our tourist destination class represents each tourist destination
 public class TouristDestination implements Serializable {
     private int imageSource;
     private String title;
@@ -14,7 +14,8 @@ public class TouristDestination implements Serializable {
         this.title = title;
         this.description = description;
     }
-
+    //set and get methods below to use when creating or
+    //setting the object as a list
     public int getImageSource() {
         return imageSource;
     }
